daadc7a8c29dee3c5e471e366329591b7f8f5b5f
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2024-04-20 18:46:14 +0530

    package created

--- Calories-Burn-Prediction/src/main/java/com/example/CaloriesBurnPrediction/CaloriesBurnPredictionApplication.java
+++ Calories-Burn-Prediction/src/main/java/com/example/CaloriesBurnPrediction/CaloriesBurnPredictionApplication.java
@@ -1,4 +1,4 @@
-package com.example.Sentiment_Analysis;
+package com.example.CaloriesBurnPrediction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Calories-Burn-Prediction/src/test/java/com/example/CaloriesBurnPrediction/CaloriesBurnPredictionApplicationTests.java
+++ Calories-Burn-Prediction/src/test/java/com/example/CaloriesBurnPrediction/CaloriesBurnPredictionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Sentiment_Analysis;
+package com.example.CaloriesBurnPrediction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
