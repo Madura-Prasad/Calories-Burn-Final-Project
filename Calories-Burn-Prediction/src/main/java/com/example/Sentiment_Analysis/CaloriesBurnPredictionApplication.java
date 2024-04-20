70f1cf4a6890128cf62fdc172c0a189f3231e8e2
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2024-04-20 18:40:36 +0530

    Project  Intiated

---
+++ Calories-Burn-Prediction/src/main/java/com/example/Sentiment_Analysis/CaloriesBurnPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sentiment_Analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesBurnPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesBurnPredictionApplication.class, args);
+	}
+
+}
